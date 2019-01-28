036b4ca84ed2ef06c7423b2e7d36149b6e2c718c
mercolino <mercolino@gmail.com>
2019-01-28 00:25:49 -0500

    changing name for application

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from wanem_app.WANEm_FrontEnd import app as application
+from wanem_app.WANEM_FrontEnd import app as application
 
 if __name__ == "__main__":
     application.run()
